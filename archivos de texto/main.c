4090da69143fdcf7c10b18cf953b6b8346b3daa4
XaeroreaX <martinpitameglia@gmail.com>
2017-07-27 17:00:19 -0300

    vamos a leer este archivo de texto

---
+++ archivos de texto/main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+        1. Parse del archivo data.csv
+        2. Listar Empleados
+        3. Ordenar por nombre
+        4. Agregar un elemento
+        5. Elimina un elemento
+        6. Listar Empleados (Desde/Hasta)
+*****************************************************/
+
+
+int main()
+{
+
+
+
+    char seguir='s';
+    int opcion=0, returnAux = DENEID;
+    FILE* pFile;
+
+    ArrayList* pArrayListEmployee = al_newArrayList();
+
+    while(seguir=='s')
+        {
+            printf("1. Parse del archivo data.csv\n");
+            printf("2. Listar Empleados\n");
+            printf("3. Ordenar por nombre\n");
+            printf("4. Agregar un elemento\n");
+            printf("5. Elimina un elemento\n");
+            printf("6. Listar Empleados (Desde/Hasta)\n");
+            printf("7. salir\n");
+
+            printf("\n\tIngrese opcion:");
+
+            scanf("%d",&opcion);
+
+            switch(opcion)
+            {
+                case 1:
+                    system("cls");
+
+
+                    system("pause");
+                    break;
+                case 2:
+                    system("cls");
+
+                    system("pause");
+                    break;
+                case 3:
+                    system("cls");
+
+                    system("pause");
+                    break;
+                case 4:
+                    system("cls");
+;
+
+                    system("pause");
+                   break;
+                case 5:
+                    system("cls");
+
+                    system("pause");
+                    break;
+                case 6:
+                    system("cls");
+
+                    system("pause");
+                    break;
+                case 7:
+                    seguir = 'n';
+                    break;
+
+
+            }
+
+         //   if(movieList->sort(movieList, compareMovie, 1) == DENEID ) printf("ERROR en la funsion sort de arrayList de EMovie");
+         //   movieListToFile(movieList);
+            system("cls");
+
+        }
+
+    return 0;
+}
---
+++ archivos de texto/parser.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+
+int parserEmployee(FILE* pFile , ArrayList* pArrayListEmployee)
+{
+    int i;
+    char auxString[128], auxChar;
+
+    Employee* auxEmployee;
+
+    pFile = fopen("data.csv","r");
+
+    if(pFile == NULL || pArrayListEmployee == NULL)
+    {
+        printf("no se pudo leer el archivo\n")
+        return -1;
+    }
+
+    fgets(auxString, 128, pFile);
+    freeString(auxString, 128);
+
+    while(!feof(pFile))
+    {
+        for(i = 0; auxChar != ","; i++)
+        {
+            auxChar = fgetc(pFile);
+
+            if(auxChar != ",")
+            {
+                auxString[i] = auxChar;
+            }
+
+        }
+
+        auxEmployee->id = atoi(auxChar);
+
+
+        for(i = 0; auxChar != ","; i++)
+        {
+            auxChar = fgetc(pFile);
+
+            if(auxChar != ",")
+            {
+                auxString[i] = auxChar;
+            }
+
+        }
+
+
+        strcpy(auxEmployee->name, auxString);
+
+
+        for(i = 0; auxChar != ","; i++)
+        {
+            auxChar = fgetc(pFile);
+
+            if(auxChar != ",")
+            {
+                auxString[i] = auxChar;
+            }
+
+        }
+
+
+        strcpy(auxEmployee->name, auxString);
+
+        fgets(auxString, 6, pFile);
+
+    }
+
+    fclose(pFile);
+
+    return 0;
+}
+
+
+
+void freeString(char String[size], int size)
+{
+    int i;
+
+    for(i = 0; i < size; i++)
+        char String[i] = '\0';
+
+}
+
+/*
+char* FgetsToChar(FILE* pFile, char to)
+{
+
+
+    for(i = 0; auxChar != ","; i++)
+    {
+        auxChar = fgetc(pFile);
+
+        if(auxChar != ",")
+        {
+            auxString[i] = auxChar;
+        }
+
+        }
+
+
+}*/
